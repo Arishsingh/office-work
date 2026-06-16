649cc2432e5360e5f6f36ccfe255c3144fb45ef4
Arishsingh <arishsingh9469@gmail.com>
2026-06-16 16:36:57 +0530

    solving deloyemnt issues

--- server/server.js
+++ server/server.js
@@ -205,4 +205,8 @@ app.get('/api/activity', auth, async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
